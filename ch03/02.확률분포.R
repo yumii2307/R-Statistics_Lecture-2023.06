8802c64d6307920a1e655cfa35df78ee00e38c34
Yumi Kim <yumii2307@naver.com>
2023-06-14 12:41:00 +0900

    ch03

--- ch03/02.확률분포.R
+++ ch03/02.확률분포.R
@@ -89,3 +89,20 @@ qnorm(0.995)            # 2.576, 99% 신뢰수준
 pnorm(2.576) - pnorm(-2.576)    # 0.99
 
 # 95% 신뢰구간 그림
+z <- seq(-3, 3, by=0.001)
+z.p <- dnorm(z)
+plot(z, z.p, axes=F, type="l", 
+     main="표준정규분포 (95%)", ylab="", ylim=c(-0.04, 0.4))
+axis(1)
+
+lines(c(-3, 3), c(0, 0))
+points(-1.96, -0.02, pch=17, col="red")
+text(-1.96, -0.035, "-1.96", col="red")
+points(1.96, -0.02, pch=17, col="red")
+text(1.96, -0.035, "1.96", col="red")
+
+s <- seq(-1.96, 1.96, by=0.001)
+s.z <- dnorm(s, mean=0, sd=1)
+s <- c(-1.96, s, 1.96)
+s.z <- c(0, s.z, 0)
+polygon(s, s.z, col="red", density=10, angle=305)
